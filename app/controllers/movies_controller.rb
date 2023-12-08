537d683a64693958259b1134aca769ff042af813
1Bishop <chris@bishop.dev>
2023-12-08 14:26:58 -0500

    Update movies list to pull from database
    The movies list in the MoviesController index action has been updated to pull from the Movie model's all method instead of from a static array. This also required adjustments in the views/movies/index.erb file to properly display the movie titles, ratings, and gross earnings pulled from the database.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    @movies = ["Iron Man", "The Incredible Hulk", "Iron Man 2", "Thor", "Captain America: The First Avenger"]
+    @movies = Movie.all()
   end
 end
