4b6ec9d564b1838437ac37571c7f39cc8f24c950
1Bishop <chris@bishop.dev>
2023-12-08 20:20:25 -0500

    Add functionality to create new movies
    Refactored routes to use resources and added methods to MoviesController for creating new movies. Also, added a "Add New Movie" button to the movies index view and created a new view for inputting the movie's details.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,16 +14,32 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
 
-    movie_params =
-      params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
-
     if @movie.update(movie_params)
       redirect_to movie_path(format: :html)
     else
       render :edit
     end
 
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movie_url(@movie.id, format: :html)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
   end
 
 end
