eae180c2e72db284df86ac6067371d062dd706aa
1Bishop <chris@bishop.dev>
2023-12-08 20:05:58 -0500

    Add movie update functionality and modify date selector
    A new method was added to the movies controller to allow for updating a movie's details. The routes were updated to include a patch route for this purpose. Furthermore, the date input field in the movie edit view has been amended to a date selector for easier user interaction.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,20 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    movie_params =
+      params.require(:movie).
+        permit(:title, :description, :rating, :released_on, :total_gross)
+
+    if @movie.update(movie_params)
+      redirect_to movie_path(format: :html)
+    else
+      render :edit
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show", as: :movie
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
   root "movies#index"
+
+  get "movies" => "movies#index"
+  get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  patch "movies/:id" => "movies#update"
 
 end
