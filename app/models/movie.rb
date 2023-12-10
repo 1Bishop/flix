7c586cac484280251ae9982ff4fb2267e087beda
1Bishop <chris@bishop.dev>
2023-12-09 19:21:55 -0500

    Add 'released' method to Movie model
    A 'released' method was added to the Movie model to fetch movies that have already been released. Also, the index action in MoviesController has been refactored to use this new method instead of getting all movies.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,9 @@ class Movie < ApplicationRecord
     total_gross.blank? || total_gross < 225_000_000
   end
 
+  def self.released
+    where("released_on < ?", Time.now).order("released_on desc")
+  end
+
 end
 
